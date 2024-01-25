271e1003964b7a6b8aa75cae64eb020bdb949f96
MaxterCodee <122041453@upq.edu.mx>
2024-01-24 20:25:47 -0600

    pruimerooooou!

---
+++ clase.py
@@ -0,0 +1,60 @@
+# comentarios de 
+# varias lineas
+
+# CADENA
+
+print('soy una cadena')
+print("segunda ocion de string")
+
+a = 'mi banda \n favorita es'
+b = "grupo marrano"
+
+print(a, b)
+print(a + b)
+
+print(len(a))
+
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+
+
+# 3 VARIABLES
+x = int(5)
+y = str("John")
+x = float(4.6)
+z = int
+
+print(type(x))
+print(y)
+
+import random 
+numero = random.randrange(1, 15)
+print(numero)
+
+# 4. SOLICITUD DE DATOS
+var1 =  input("Introduce cadena de texto:")
+var2 =  str(input("Introduce cadena de texto:"))
+var3 =  int(input("Introduce entero:"))
+var4 =  float(input("Introduce decimal:"))
+
+print(var1, var2, var3, var4) 
+
+# 5 BOOLEANS, OPERADORES DE COMPARACION
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+print(10 >= 9)
+print(10 <= 9)
+print(10 != 9)
+
+
+# 6. OPERADORES LOGICOS
+
+x = 1
+
+print(x < 5 & x < 10)
+print(x < 5 or x < 10)
+print(not(x < 5 and x < 10))
+
